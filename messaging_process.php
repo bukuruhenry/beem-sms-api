4997090e0b6602967b488a9dccd1362fe84a3a8e
Henry Bukuru <30518721+bukuruhenry@users.noreply.github.com>
2021-06-17 14:32:52 +0300

    Update messaging_process.php

--- messaging_process.php
+++ messaging_process.php
@@ -5,14 +5,13 @@ require_once 'includes/functions.php';
 $full_name = array();
 $phone_number = array(); //Used in user_table.php file
 
-$sql_users = mysqli_query($con, "SELECT fullname,phonenumber FROM users"); //Assuming I already have a database with a users table
-
+$sql_users = mysqli_query($con, "SELECT fullname,phonenumber FROM users"); //Assuming I already have a database with a users table and $con is defined in config.php file
 
 // Post Request from messaging.js file
 
 if (isset($_POST["users"])) {
 
-	$value = json_decode($_POST["users"]);
+    $value = json_decode($_POST["users"]);
     $count_values    = count($value->phonenumber_item);
     
     for ($i = 0; $i < $count_values;) {

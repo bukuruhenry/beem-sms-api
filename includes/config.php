2b0120c6eebd6f408ec6d9b1ea306e93679d5e17
Henry Bukuru <30518721+bukuruhenry@users.noreply.github.com>
2021-06-17 14:25:02 +0300

    Create config.php

---
+++ includes/config.php
@@ -0,0 +1,4 @@
+// Connect Your DB, declare variables and retrieve all data from database that may be used mostly
+<?php 
+ $con = mysqli_connect('server','database_user','user_password','database_name');  
+?>
